97b84e01a301ab1ac3bf5b7eeecdb6c90bbaf9fe
alex-greff <alex.j.greff@gmail.com>
2020-03-14 20:20:21 -0400

    Moved out room manager cleanup to RoomStatus. Cleaned up Room view code

--- client/src/components/sticky/RoomStatus.vue
+++ client/src/components/sticky/RoomStatus.vue
@@ -8,10 +8,64 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import { mapGetters, mapActions } from "vuex";
+import * as RoomStore from "../../store/modules/room";
+import * as AudioStore from "../../store/modules/audio";
+import RoomManager from '../../rtc/RoomManager';
+import SignallingSocket from '../../socket/SignallingSocket';
+import VueRouter from 'vue-router';
 
+type Computed = {
+
+} & Pick<RoomStore.MapGettersStructure,
+    RoomStore.Getters.isConnected
+    | RoomStore.Getters.roomManager
+>
+
+type Methods = {
+    setupSignallingSocketListeners(signallingSocket: SignallingSocket): void;
+    onRoomLeft(): void;
+} & Pick<RoomStore.MapActionsStructure,
+    RoomStore.Actions.deleteRoomManager
+>;
 
 export default Vue.extend({
-    
+    computed: {
+        ...mapGetters({
+            isConnected: RoomStore.Getters.isConnected,
+            roomManager: RoomStore.Getters.roomManager,
+        })
+    },
+    methods: {
+        ...mapActions({
+            deleteRoomManager: RoomStore.Actions.deleteRoomManager
+        }),
+        setupSignallingSocketListeners(signallingSocket: SignallingSocket) {
+            signallingSocket.on("room-left", () => {
+                const { onRoomLeft }: Methods = this;
+                onRoomLeft();
+            });
+        },
+        onRoomLeft() {
+            const { deleteRoomManager }: Methods = this;
+            const router = this.$router as VueRouter;
+
+            deleteRoomManager();
+
+            // Go back to home page
+            router.push('/');
+        }
+    },
+    watch: {
+        isConnected(connection: boolean) {
+            if (!connection) return;
+
+            // Connection established
+            const { setupSignallingSocketListeners }: Methods = this;
+            const roomManager = this.roomManager as RoomManager;
+            setupSignallingSocketListeners(roomManager.signallingSocket);
+        }
+    }
 });
 </script>
 
--- client/src/views/room/Room.vue
+++ client/src/views/room/Room.vue
@@ -94,12 +94,6 @@ import { HighResolutionTimer } from "../../utilities";
 
 interface Data {
     sendClientId: string;
-
-    // isPlaying: boolean;
-
-    // audioFile: File | null;
-    // loadedAudio: boolean;
-
     syncOffset: number;
 }
 
@@ -118,16 +112,11 @@ type Computed = {} &
         | AudioStore.Getters.audioLoaded
     >
 
-type Methods = Pick<RoomStore.MapActionsStructure, RoomStore.Actions.deleteRoomManager> & {
+type Methods = {
     leaveRoom(): void;
-    onRoomLeft(): void;
-    setupGeneralRoomListeners(roomManager: RoomManager): void;
-    setupGeneralRTCListeners(peerManager: PeerManager): void;
-    setupGeneralSocketListeners(socket: SignallingSocket): void;
     setupGeneralTimesyncListeners(timesync: Timesync): void;
 
     onAudioFileChange(): void;
-    // loadAudioFile(audioFile: File): void;
     syncAudioFile(audioFile: File): void;
     playAudio(): void;
     pauseAudio(): void;
@@ -143,11 +132,6 @@ export default Vue.extend({
     data() {
         return {
             sendClientId: "",
-            // isPlaying: false,
-
-            // audioFile: null,
-            // loadedAudio: false,
-
             syncOffset: 0,
         }
     },
@@ -171,17 +155,11 @@ export default Vue.extend({
             console.log("WARNING: Not connected to a room... you shouldn't be in here");
         } else {
             const { 
-                setupGeneralRoomListeners, 
-                setupGeneralRTCListeners, 
-                setupGeneralSocketListeners, 
                 setupGeneralTimesyncListeners }: Methods = this;
             const roomManager = this.roomManager as RoomManager;
             const peerManager = roomManager.peerManager as PeerManager;
             const signallingSocket = roomManager.signallingSocket as SignallingSocket;
 
-            setupGeneralRoomListeners(roomManager);
-            setupGeneralRTCListeners(peerManager);
-            setupGeneralSocketListeners(signallingSocket);
             setupGeneralTimesyncListeners(peerManager.timesync);
         }
     },
@@ -193,96 +171,12 @@ export default Vue.extend({
             const roomManager = this.roomManager as RoomManager;
             roomManager.leaveRoom();
         },
-        onRoomLeft() {
-            const { deleteRoomManager }: Methods = this;
-            const router = this.$router as VueRouter;
-
-            deleteRoomManager();
-
-            // Go back to home page
-            router.push('/');
-        },
-        setupGeneralRoomListeners(roomManager: RoomManager) {
-            // roomManager.addEventListener("audiofilereceived", (audioFile) => {
-            //     console.log("Received audio file", audioFile);
-
-            //     const audioPlayerEl = this.$refs.audioPlayerEl as HTMLAudioElement;
-
-            //     audioPlayerEl.src = URL.createObjectURL(audioFile);
-            //     audioPlayerEl.load();
-
-            //     this.loadedAudio = true;
-            // });
-
-            // roomManager.addEventListener("playsignalreceived", startTime => {
-            //     console.log("Start signal received for", startTime);
-
-            //     const roomManager = this.roomManager as RoomManager;
-            //     const peerManager = roomManager.peerManager as PeerManager;
-            //     // const now = peerManager.timesync.now();
-            //     const now = Date.now();
-
-            //     // TODO: might have to check for null peer manager
-
-            //     // const delay = startTime - Math.abs(now); // TODO: remove the abs hack
-            //     // console.log("Timer delay", delay); // TODO: remove
-            //     // const timer = new HighResolutionTimer(delay, () => {
-            //     //     const audioPlayerEl = this.$refs.audioPlayerEl as HTMLAudioElement;
-            //     //     if (!audioPlayerEl.src) return;
-
-            //     //     console.log("Playing audio"); // TODO: remove
-            //     //     audioPlayerEl.play();
-            //     //     this.isPlaying = true;
-            //     //     timer.stop();
-            //     // });
-            //     // timer.run();
-
-            //     // TODO: actually use start time signal instead of just playing when the signal arrives
-            //     const audioPlayerEl = this.$refs.audioPlayerEl as HTMLAudioElement;
-            //     if (!audioPlayerEl.src) return;
-
-            //     console.log("Playing audio"); // TODO: remove
-            //     audioPlayerEl.play();
-            //     this.isPlaying = true;
-            // });
-
-            // roomManager.addEventListener("pausesignalreceived", sentTime => {
-            //     console.log("Pause signal received, sent at", sentTime);
-
-            //     const audioPlayerEl = this.$refs.audioPlayerEl as HTMLAudioElement;
-            //     if (!audioPlayerEl.src) return;
-
-            //     audioPlayerEl.pause();
-            // });
-
-            // roomManager.addEventListener("stopsignalreceived", sentTime => {
-            //     console.log("Stop signal received, sent at", sentTime);
-
-            //     const audioPlayerEl = this.$refs.audioPlayerEl as HTMLAudioElement;
-            //     if (!audioPlayerEl.src) return;
-
-            //     audioPlayerEl.pause();
-            //     audioPlayerEl.load();
-            // });
-        },
-        setupGeneralRTCListeners(peerManager: PeerManager) {
-            // peerManager.addEventListener("rtcreceivechannelmessage", ({ clientId, sourceEvent }) => {
-            //     console.log("Message from", clientId, sourceEvent.data); // TODO: remove
-            // });
-        },
-        setupGeneralSocketListeners(socket: SignallingSocket) {
-            socket.on("room-left", () => {
-                const { onRoomLeft }: Methods = this;
-                onRoomLeft();
-            });
-        },
         setupGeneralTimesyncListeners(timesync: Timesync) {
             timesync.on("change", offset => {
                 this.syncOffset = offset;
             });
         },
         onAudioFileChange() {
-            // const { loadAudioFile, syncAudioFile }: Methods = this;
             const { syncAudioFile, stopAudio }: Methods = this;
             const audioFileInputEl = this.$refs.audioFileInputEl as HTMLInputElement;
 
@@ -290,22 +184,10 @@ export default Vue.extend({
 
             if (audioFile) {
                 stopAudio();
-
-                // loadAudioFile(audioFile);
                 syncAudioFile(audioFile);
             }
         },
-        // loadAudioFile(audioFile: File) {
-        //     const audioPlayerEl = this.$refs.audioPlayerEl as HTMLAudioElement;
-
-        //     audioPlayerEl.src = URL.createObjectURL(audioFile);
-        //     audioPlayerEl.load();
-
-        //     this.loadedAudio = true;
-        // },
         syncAudioFile(audioFile: File) {
-            // TODO: sync to other clients
-            const { connectedRTCClients }: Computed = this; 
             const roomManager = this.roomManager as RoomManager;
 
             roomManager.syncAudioFile(audioFile);
@@ -313,52 +195,17 @@ export default Vue.extend({
         playAudio() {
             const roomManager = this.roomManager as RoomManager;
             const peerManager = roomManager.peerManager as PeerManager;
-            // const audioPlayerEl = this.$refs.audioPlayerEl as HTMLAudioElement;
-            // const now = peerManager.timesync.now();
-            const now = Date.now();
-
-            // TODO: might have to check for null peer manager
-
-            // if (!audioPlayerEl.src) return;
-
+            
+            // TODO: figure out proper delay timing
             const startTime = roomManager.sendPlaySignal(100);
-
-            // const delay = startTime - Math.abs(now); // TODO: remove the abs hack
-            // console.log("Timer delay", delay); // TODO: remove
-            // const timer = new HighResolutionTimer(delay, () => {
-            //     console.log("Playing audio"); // TODO: remove
-            //     audioPlayerEl.play();
-            //     this.isPlaying = true;
-            //     timer.stop();
-            // });
-            // timer.run();
-
-            // TODO: delay start time instead of just playing right away
-            // audioPlayerEl.play();
-            // this.isPlaying = true;
         },
         pauseAudio() {
             const roomManager = this.roomManager as RoomManager;
-            // const audioPlayerEl = this.$refs.audioPlayerEl as HTMLAudioElement;
-
-            // if (!audioPlayerEl.src) return;
-
             roomManager.sendPauseSignal();
-
-            // audioPlayerEl.pause();
-            // this.isPlaying = false;
         },
         stopAudio() {
             const roomManager = this.roomManager as RoomManager;
-            // const audioPlayerEl = this.$refs.audioPlayerEl as HTMLAudioElement;
-
-            // if (!audioPlayerEl.src) return;
-
             roomManager.sendStopSignal();
-
-            // audioPlayerEl.pause();
-            // audioPlayerEl.load();
-            // this.isPlaying = false;
         }
     }
 });
