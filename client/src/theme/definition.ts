da73947a99e4f9fa375e0b1ce3d75f8af1167fa0
alex-greff <alex.j.greff@gmail.com>
2020-03-18 13:17:29 -0400

    Setup theme schema and implemented classic theme

--- client/src/theme/definition.ts
+++ client/src/theme/definition.ts
@@ -1,14 +1,90 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import { Schema, Mixins } from "themer";
 
 export const schema: Schema = {
     GLOBAL: {
-        test: {
-            $type: "color",
-            $required: true
+        background_color: {
+            $mixins: ['primary_modifier', 'secondary_modifier']
+        },
+        text_color: {
+            $mixins: ['primary_modifier', 'secondary_modifier']
+        },
+        text_color_inverted: {
+            $mixins: ['primary_modifier', 'secondary_modifier']
+        },
+        accent_color: {
+            $mixins: ['primary_modifier', 'secondary_modifier', 'tertiary_modifier']
+        },
+        background_gradient_color: {
+            $mixins: ['start_modifier', 'end_modifier']
+        },
+        gradient_color: {
+            $mixins: ['start_modifier', 'end_modifier']
+        },
+        error_color: {
+            $mixins: ['background_modifier', 'text_modifier', 'accent_modifier']
         }
+    },
+    // Components
+    AudioVisualizer: {
+        $inherits: "GLOBAL"
     }
 };
 
 export const mixins: Mixins = {
-
+    // --- Modifiers ---
+    primary_modifier: {
+        primary: {
+            $type: "color",
+            $required: true
+        }
+    },
+    secondary_modifier: {
+        secondary: {
+            $type: "color",
+            $required: true
+        }
+    },
+    tertiary_modifier: {
+        tertiary: {
+            $type: "color",
+            $required: true
+        },
+    },
+    quaternary_modifier: {
+        quaternary: {
+            $type: "color",
+            $required: true
+        }
+    },
+    start_modifier: {
+        start: {
+            $type: "color",
+            $required: true
+        }
+    },
+    end_modifier: {
+        end: {
+            $type: "color",
+            $required: true
+        }
+    },
+    background_modifier: {
+        background: {
+            $type: "color",
+            $required: true
+        }
+    },
+    text_modifier: {
+        text: {
+            $type: "color",
+            $required: true
+        }
+    },
+    accent_modifier: {
+        accent: {
+            $type: "color",
+            $required: true
+        }
+    }
 };
--- client/src/theme/themes/classic.ts
+++ client/src/theme/themes/classic.ts
@@ -1,8 +1,66 @@
+/* eslint-disable @typescript-eslint/camelcase */
+const LIGHT_1 = "#FFFFFF";
+const LIGHT_2 = "#FCF8FF";
+
+const DARK_1 = "#000000";
+const DARK_2 = "#180037";
+
+const PURPLE_1 = "#B072FE";
+const PURPLE_2 = "#C06CE8";
+const PURPLE_3 = "#D274FF";
+
+
+const PINK_1 = "#F1B8FF";
+const PINK_2 = "#FFADFC";
+const PINK_3 = "#F9D6FF";
+const PINK_4 = "#FC7AFF";
+
+const BLUE_1 = "#87FFF8";
+
+const ERROR_BG = "#FFF9F9";
+const ERROR_TEXT = DARK_1;
+const ERROR_ACCENT = "#DE1919";
+
 export default {
     name: "classic",
     theme: {
         GLOBAL: {
-            test: "#9EDFE6"
+            background_color: {
+                primary: LIGHT_1,
+                secondary: LIGHT_2
+            },
+            text_color: {
+                primary: DARK_1,
+                secondary: DARK_2
+            },
+            text_color_inverted: {
+                primary: LIGHT_1,
+                secondary: LIGHT_1 // TODO: pick a different color
+            },
+            accent_color: {
+                primary: PURPLE_1,
+                secondary: PINK_1,
+                tertiary: PINK_2
+            },
+            background_gradient_color: {
+                start: LIGHT_1,
+                end: PINK_3
+            },
+            gradient_color: {
+                start: PURPLE_2,
+                end: PINK_4
+            },
+            error_color: {
+                background: ERROR_BG,
+                text: ERROR_TEXT,
+                accent: ERROR_ACCENT
+            }
+        },
+        AudioVisualizer: {
+            gradient_color: {
+                start: PURPLE_3,
+                end: BLUE_1
+            }
         }
     }
 }
