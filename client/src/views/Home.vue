6ac7e9626a0bb1805843f0634212e6d1392e08b4
alex-greff <alex.j.greff@gmail.com>
2020-03-20 01:16:41 -0400

    Added background to logo text when crowd image reaches it on larger resolutions

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -5,14 +5,16 @@
             :style="crowdImageStyles"
         ></div>
 
-        <div id="Home__logo">
-            <div id="Home__app-name">
-                <span id="Home__name-light">Sync&nbsp;</span>
-                <span id="Home__name-bold">Tune</span>
-            </div>
-            <div id="Home__app-slogan">
-                <span id="Home__slogan-light">Extend Your</span>
-                <span id="Home__slogan-bold">&nbsp;Sound</span>
+        <div id="Home__logo-container">
+            <div id="Home__logo">
+                <div id="Home__app-name">
+                    <span id="Home__name-light">Sync&nbsp;</span>
+                    <span id="Home__name-bold">Tune</span>
+                </div>
+                <div id="Home__app-slogan">
+                    <span id="Home__slogan-light">Extend Your</span>
+                    <span id="Home__slogan-bold">&nbsp;Sound</span>
+                </div>
             </div>
         </div>
 
@@ -75,65 +77,92 @@ export default {
 
             pointer-events: none;
 
+            z-index: 0;
+
             @keyframes image-hue-anim {
                 $opacity-amount: 80%;
+                $brightness-amount: 80%;
 
                 0% {
-                    filter: hue-rotate(0deg) opacity($opacity-amount);
+                    filter: hue-rotate(0deg) opacity($opacity-amount) brightness($brightness-amount);
                 }
                 50% {
-                    filter: hue-rotate(360deg) opacity($opacity-amount);
+                    filter: hue-rotate(360deg) opacity($opacity-amount) brightness($brightness-amount);
                 }
                 100% {
-                    filter: hue-rotate(0deg), opacity($opacity-amount);
+                    filter: hue-rotate(0deg), opacity($opacity-amount) brightness($brightness-amount);
                 }
             }
         }
 
-        & #Home__logo {
-            padding: 0 1.5rem;
+        & #Home__logo-container {
+            width: 75%;
+            $final-opacity: 0.6;
+            $cutoff: 45%;
 
-            & #Home__app-name {
-                $font-size: 7rem;
+            background: rgb(0,0,0);
+            background: linear-gradient(90deg, rgba(0,0,0,0) 0%, rgba(0,0,0,$final-opacity) $cutoff, rgba(0,0,0,$final-opacity) 100% - $cutoff, rgba(0,0,0,0) 100%);
 
-                display: flex;
-                flex-direction: row;
-                justify-content: center;
-                flex-wrap: wrap;
+            color: color-link("Home", "text_inverted", "primary");
 
-                font-size: $font-size;
-                line-height: $font-size - 0.7rem;
+            @include respond(tab-port) {
+                width: auto;   
+                background: none;
 
-                white-space: pre-wrap;
+                color: color-link("Home", "text", "primary");
+            }
 
-                @include respond(phone) {
-                    $font-size: 6rem;
+            display: flex;
+            flex-direction: row;
+            align-content: center;
+            justify-content: center;
+
+            z-index: 1;
+
+            & #Home__logo {
+                padding: 0 1.5rem;
+
+                & #Home__app-name {
+                    $font-size: 7rem;
+
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: center;
+                    flex-wrap: wrap;
+
                     font-size: $font-size;
                     line-height: $font-size - 0.7rem;
+
+                    white-space: pre-wrap;
+
+                    @include respond(phone) {
+                        $font-size: 6rem;
+                        font-size: $font-size;
+                        line-height: $font-size - 0.7rem;
+                    }
                 }
-            }
 
-            & #Home__app-slogan {
-                display: flex;
-                flex-direction: row;
-                justify-content: flex-end;
-                flex-wrap: wrap;
+                & #Home__app-slogan {
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: flex-end;
+                    flex-wrap: wrap;
 
-                white-space: pre-wrap;
-            }
+                    white-space: pre-wrap;
+                }
 
-            & #Home__name-light, & #Home__slogan-light {
-                color: color-link("Home", "text", "primary");
-                font-weight: 400;
-            }
+                & #Home__name-light, & #Home__slogan-light {
+                    font-weight: 400;
+                }
 
-            & #Home__name-bold, & #Home__slogan-bold {
-                color: color-link("Home", "accent", "tertiary");
-                font-weight: 600;
-            }
+                & #Home__name-bold, & #Home__slogan-bold {
+                    color: color-link("Home", "accent", "tertiary");
+                    font-weight: 600;
+                }
 
-            & #Home__slogan-bold, & #Home__slogan-light {
-                text-align: right;
+                & #Home__slogan-bold, & #Home__slogan-light {
+                    text-align: right;
+                }
             }
         }
 
