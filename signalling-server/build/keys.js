9a584e76be2d266c4dbf93eeca1ed048ddd30e58
alex-greff <alex.j.greff@gmail.com>
2020-03-22 14:01:49 -0400

    Updated .gitignore to ignore build directories

--- signalling-server/build/keys.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    SOCKET_IO_PATH: (process.env.SOCKET_IO_PATH || 'socket.io')
-};
-//# sourceMappingURL=keys.js.map
