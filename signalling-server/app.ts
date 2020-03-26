3d9e20127269a4052ee8d9e143d88e49bba47a9d
Anton <antonwizkam@gmail.com>
2020-03-26 11:52:45 -0400

    deploy to heroku, set up CORS

--- signalling-server/app.ts
+++ signalling-server/app.ts
@@ -1,13 +1,18 @@
 import * as http from "http";
 import express, { Request, Response, NextFunction } from "express";
+import KEYS from "./keys";
 import signalling from "./src/io/signalling";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.get("/test", (req, res) => res.end("It works!"));
+app.use((req, res, next) => {
+    console.log("HTTP request", req.method, req.url);
+    res.header("Access-Control-Allow-Origin", KEYS.CLIENT_HOST_PATH);
+    next();
+});
 const server = http.createServer(app);
 server.listen(PORT);
 
 // Setup signalling server
 signalling(server);
-
--- signalling-server/keys.ts
+++ signalling-server/keys.ts
@@ -1,3 +1,4 @@
 export default {
-    SOCKET_IO_PATH: (process.env.SOCKET_IO_PATH || 'socket.io') as string
+    SOCKET_IO_PATH: (process.env.SOCKET_IO_PATH || 'socket.io') as string,
+    CLIENT_HOST_PATH: (process.env.CLIENT_HOST_PATH || '/') as string
 }
