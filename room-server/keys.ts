2d548f21bbb4a337cf1584d014b58a7001743c7c
Anton <antonwizkam@gmail.com>
2020-03-27 18:39:39 -0400

    clean up some env vars

--- room-server/keys.ts
+++ room-server/keys.ts
@@ -1,5 +1,4 @@
 export default {
-    SOCKET_IO_PATH: (process.env.SOCKET_IO_PATH || "socket.io") as string,
     REDIS_URL: (process.env.REDIS_URL || "redis://redis") as string,
     SESSION_SECRET: (process.env.SESSION_SECRET || "SUPERSECRET") as string,
     IS_PROD: (process.env.NODE_ENV === "production") as boolean,
