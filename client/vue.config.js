3f143d226f37488ee0145cf06c09b9d55f245940
alex-greff <alex.j.greff@gmail.com>
2020-04-05 12:47:07 -0400

    Trying to fix build issue

--- client/vue.config.js
+++ client/vue.config.js
@@ -10,13 +10,14 @@ module.exports = {
     // Source: https://github.com/vuejs/vue-cli/issues/3603#issuecomment-483913563
     chainWebpack: config => {
         // remove vue-cli-service's progress output
-        config.plugins.delete('progress');
-        // optionally replace with another progress output plugin
-        config.plugin('simple-progress-webpack-plugin').use(require.resolve('simple-progress-webpack-plugin'), [
-            {
-            format: 'minimal', // options are minimal, compact, expanded, verbose
-            },
-        ]);
+        // TODO: put back in
+        // config.plugins.delete('progress');
+        // // optionally replace with another progress output plugin
+        // config.plugin('simple-progress-webpack-plugin').use(require.resolve('simple-progress-webpack-plugin'), [
+        //     {
+        //     format: 'minimal', // options are minimal, compact, expanded, verbose
+        //     },
+        // ]);
     },
     css: {
         loaderOptions: {
