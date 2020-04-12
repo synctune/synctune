68a000f1b14402d150969fac2662857fe2ee25d6
Anton <antonwizkam@gmail.com>
2020-04-12 15:25:49 -0400

    add validation to room server

--- room-server/src/routes/RoomRoute.ts
+++ room-server/src/routes/RoomRoute.ts
@@ -2,16 +2,26 @@ import { Request, Response, NextFunction } from "express";
 import { createClient } from "redis";
 import crypto from "crypto";
 import KEYS from "../../keys";
+import { isString } from "util";
 
 const A_DAY = 86400;
 const redisClient = createClient(KEYS.REDIS_URL);
 
+const roomNameInvalid = (roomName: String) =>
+    roomName === null || !isString(roomName) || roomName.length !== 9;
+const peerIdInvalid = (peerId: String) =>
+    peerId === null || !isString(peerId) || peerId.length === 0;
+
 // @param roomName
 // @500 on error returns the error
+// @400 if roomName is malformed
 // @404 if room doesn't exist
 // @200 ownerId: peerid of host
 export const getRoomOwnerPeerId = (req: Request, res: Response) => {
     const roomName = req.params.roomName;
+    if (roomNameInvalid(roomName)) {
+        return res.status(400).end("Malformed roomName");
+    }
     redisClient.hgetall(roomName, (err, roomData) => {
         if (err) {
             console.log("getRoomOwner Redis error:", err);
@@ -24,10 +34,16 @@ export const getRoomOwnerPeerId = (req: Request, res: Response) => {
 };
 
 // @body roomName, selfId
+// @400 if roomName is malformed or peerid malformed
 // @409 if room exists already
 // @200 on success
 export const createRoom = (req: Request, res: Response) => {
     const { roomName, selfId } = req.body;
+    if (roomNameInvalid(roomName)) {
+        return res.status(400).end("Malformed roomName");
+    } else if (peerIdInvalid(selfId)) {
+        return res.status(400).end("Malformed selfId");
+    }
     const secret = crypto.randomBytes(20).toString("hex");
     redisClient.exists(roomName, (err, exists) => {
         if (err) {
@@ -65,12 +81,16 @@ export const createRoom = (req: Request, res: Response) => {
 };
 
 // @param roomName
+// @400 if roomName is malformed
 // @500 on error returns the error
 // @404 if room doesn't exist
 // @501 if the requester is not the room owner
 // @200 on success
 export const closeRoom = (req: Request, res: Response) => {
     const { roomName } = req.params;
+    if (roomNameInvalid(roomName)) {
+        return res.status(400).end("Malformed roomName");
+    }
     // query redis for room name
     redisClient.hgetall(roomName, (err, roomData) => {
         if (err) {
