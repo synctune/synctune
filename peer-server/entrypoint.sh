f3f4fee60a81805cf4e4926f1b0fc859cbe41a7e
alex-greff <alex.j.greff@gmail.com>
2020-09-14 00:24:24 -0400

    Removed certbot stuff for now

--- peer-server/entrypoint.sh
+++ peer-server/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-export CERT_PATH=/peerjs-ssl/peerjs.cert
-export KEY_PATH=/peerjs-ssl/peerjs.key
+# export CERT_PATH=/peerjs-ssl/peerjs.cert
+# export KEY_PATH=/peerjs-ssl/peerjs.key
 
-certbot certonly --standalone --agree-tos --cert-path "$CERT_PATH" --key-path "$KEY_PATH" --email antonwizkam@gmail.com --non-interactive
+# certbot certonly --standalone --agree-tos --cert-path "$CERT_PATH" --key-path "$KEY_PATH" --email antonwizkam@gmail.com --non-interactive
 
-node bin/peerjs --port 443 --sslkey "$KEY_PATH" --sslcert "$CERT_PATH"
+# node bin/peerjs --port 443 --sslkey "$KEY_PATH" --sslcert "$CERT_PATH"
+
+node bin/peerjs --port 443
